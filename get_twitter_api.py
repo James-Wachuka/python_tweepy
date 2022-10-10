44fe8c1781ac47f369ff7eb55b7a41bf0226e16b
James Mwangi <jewachu26@gmail.com>
2022-10-10 15:31:26 +0300

    Update get_twitte_api

--- get_twitter_api.py
+++ get_twitter_api.py
@@ -2,10 +2,10 @@ import tweepy
 
 def twitter_api():
     # input your twitter API credentials
-    consumer_key = "CvZaGFcvsOdgg1vOr7kClZAgQ"
-    consumer_secret = "NYedUoy4ksrlVyAo47EJnF5ZHY45spl3tW43U38LCkv0SitLmO"
-    access_token = "1357221708541939714-SDICyG3wVh6BbnZ73voX1vqPeMpk1l"
-    access_token_secret = "prqlcC2XzVfbZs3YUasg3IhSWYrcDoGZgcvqdjJubHYol"
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
     # authentication of consumer key and secret
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     # authentication of access token and secret
